18cddbbeb5bf873efb3f285739e666d9ade99465
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2023-09-04 12:01:28 -0700

    cycle func

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * check_cycle - Check if a linked list has a cycle
+ * @list: Pointer to the head of the linked list
+ *
+ * Return: 1 if there is a cycle, 0 if not
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *fast;
+	listint_t *slow;
+
+	fast = list;
+	slow = list;
+
+	while ((fast != NULL) && (fast->next != NULL))
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
