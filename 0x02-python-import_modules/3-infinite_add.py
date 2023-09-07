c3a37d03b70e5fb52fa01f230b33336a832dd792
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2023-09-07 16:31:56 -0700

    updating taks 4

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,10 @@
-to edit 
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Infinite add all the arguments"""
+    import sys
+
+    sum = 0
+    for i in range(1, len(sys.argv)):
+        sum += int(sys.argv[i])
+    print("{}".format(sum))
