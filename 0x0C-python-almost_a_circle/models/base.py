e29cc1cb16828dc50f8422ba1cae05fc865d9934
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2023-10-13 12:00:29 -0700

    task 1 pycodestyle update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
+"""almost_a_circle"""
+
 
 class Base:
     """
     A base class for object management.
-
     This class provides a mechanism for managing object identifiers.
-
     Attributes:
-        __nb_objects (int): A private class variable representing the number of created objects.
-
+        __nb_objects (int): A private class variable.
     Methods:
         __init__: Initializes an object with a unique identifier.
     """
@@ -18,16 +17,14 @@ class Base:
     def __init__(self, id=None):
         """
         Initializes an object with a unique identifier.
-
-        If an identifier is provided, it is assigned to the object. Otherwise, a unique identifier is generated and assigned.
-
+        If an identifier is provided, it is assigned to the object.
         Args:
-            id (int, optional): An identifier for the object. Defaults to None.
+            id (int, optional): An identifier for the object.
         """
+
         self.id = id
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
