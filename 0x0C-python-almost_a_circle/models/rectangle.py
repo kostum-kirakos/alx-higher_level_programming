9eba2c5ad0d461dc1f1c45a5665106d4ba3253ea
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2023-10-14 10:40:43 -0700

    update method commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -105,3 +105,16 @@ class Rectangle(Base):
         i = self.id
         string = "[{}] ({}) {}/{} - {}/{}".format(rec, i, x, y, w, h)
         return string
+
+    def update(self, *args):
+        """update_method"""    
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.__width = args[1]
+        if len(args) >= 3:
+            self.__height = args[2]
+        if len(args) >= 4:
+            self.__x = args[3]
+        if len(args) >= 5:
+            self.__y = args[4]
