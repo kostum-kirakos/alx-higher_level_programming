ea1f5b1562bcbfdb85c2ef3e61ef41333f6de39d
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2023-12-12 12:44:12 -0800

    prints x times a string

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@ const argc = process.argv;
 let index;
 if (+(argc[2])) {
   for (index = 0; index < (+(argc[2])); index++) {
-    console.log('c is fun');
+    console.log('C is fun');
   }
 } else {
   console.log('Missing number of occurrences');
