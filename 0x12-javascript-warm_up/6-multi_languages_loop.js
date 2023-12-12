94704a24def5ee712b0eb742e7661b288cad1ac0
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2023-12-12 12:30:21 -0800

    printing strings in array

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let index;
+for (index = 0; index < myArr.length; index++) {
+  console.log(`${myArr[index]}`);
+}
