4f2d90dcb20a9792fee85c0ed7e2e92c53fdfaf5
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2023-12-13 08:54:53 -0800

    printing the rectangle using a method

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    if (this.width && this.height) {
+      for (let index1 = 0; index1 < this.height; index1++) {
+        for (let index2 = 0; index2 < this.width; index2++) {
+          process.stdout.write('X');
+        }
+        console.log();
+      }
+    }
+  }
+}
+module.exports = Rectangle;
