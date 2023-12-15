fbfc860b569a8bc5d06d2ade3a68c50e8ce20acc
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2023-12-15 09:46:47 -0800

    list all data there names starting with a big N of state tables

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+
+    curr = conn.cursor()
+    curr.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    result = curr.fetchall()
+    for row in result:
+        print(row)
+    curr.close()
+    conn.close()
