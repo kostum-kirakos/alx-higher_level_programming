e302c7a9e149da7b26daa78e1d97d399c1126a04
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2023-12-15 10:03:06 -0800

    list all data there names is the 4th argument of state tables

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+
+    curr = conn.cursor()
+    curr.execute("""SELECT * FROM states WHERE name
+                 LIKE '{}%' ORDER BY states.id""".format(sys.argv[4]))
+    result = curr.fetchall()
+    for row in result:
+        print(row)
+    curr.close()
+    conn.close()
