e3d27e56471b7df729ae60a5ff19106955e5514a
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2024-01-06 09:06:35 -0800

    Write a Bash script that takes in a URL and displays all HTTP methods the server will accept.

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sI -X OPTIONS "$1" | grep -s 'allow' | awk '{print $2}'
+curl -sI -X OPTIONS "$1" | grep -i 'allow' | awk '{print $2}'
