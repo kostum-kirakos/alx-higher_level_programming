8aba4115469d88f0737e17d3e4e3e32061ff9714
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2024-01-06 08:44:32 -0800

    A script that sends a GET request to a URL

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+if [ -z "$1" ]; then
+	echo "Usage: $0 <URL>"
+	exit 1
+fi
+
+curl -s "$1"
